c95cb27f624a1423cdd99c74c0566318843c7c71
fidhafathima00 <165751210+fidhafathima00@users.noreply.github.com>
2024-04-05 16:51:56 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,8 +1,17 @@
-import { combineReducers } from 'redux';
-import userReducer from './userReducer';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
 
-const rootReducer = combineReducers({
-  user: userReducer,
-});
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
 
-export default rootReducer;
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
